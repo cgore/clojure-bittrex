5b5d8918fe9a8ec7121fbe4a3f72d67ef7b0f7f5
Christopher Mark Gore <cgore@cgore.com>
2017-11-21 22:50:28 -0600

    project de-boilerplate

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject bittrex "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.cgore/bittrex "0.1.0-SNAPSHOT"
+  :description "Clojure interface to the Bittrex.com API"
+  :url "https://github.com/cgore/clojure-bittrex"
   :license {:name "BSD-3-Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
