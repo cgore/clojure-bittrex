723905799c66413da9e595becafa6de02456a7e7
Christopher Mark Gore <cgore@cgore.com>
2017-11-21 23:04:09 -0600

    Signatures for the public API

---
+++ src/bittrex/api/public.clj
@@ -0,0 +1,15 @@
+(ns bittrex.api.public)
+
+(defn get-markets [])
+
+(defn get-currencies [])
+
+(defn get-ticker [market])
+
+(defn get-market-summaries [])
+
+(defn get-market-summary [market])
+
+(defn get-order-book [market type])
+
+(defn get-market-history [market])
