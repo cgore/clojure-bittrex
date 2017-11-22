1652e5b859c1f27037c8a1b5a029b6d26d34bcf0
Christopher Mark Gore <cgore@cgore.com>
2017-11-21 22:57:41 -0600

    goodbye world

--- src/bittrex/core.clj
+++ src/bittrex/core.clj
@@ -1,6 +1,1 @@
 (ns bittrex.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/bittrex/core_test.clj
+++ test/bittrex/core_test.clj
@@ -1,7 +1,3 @@
 (ns bittrex.core-test
   (:require [clojure.test :refer :all]
             [bittrex.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
