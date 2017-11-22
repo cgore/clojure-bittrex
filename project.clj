f776034e3b6975d7d12b004ece939f30e876b5c9
Christopher Mark Gore <cgore@cgore.com>
2017-11-21 22:45:14 -0600

    License change to BSD 3-Clause, because that's what I like.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bittrex "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD-3-Clause"
+            :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
