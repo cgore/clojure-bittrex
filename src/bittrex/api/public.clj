cac9aa2b6705bba2f46a90b171d9c53b08c4ebda
Christopher Mark Gore <chris.gore@climate.com>
2017-12-01 16:44:57 -0600

    more generic uri construction

--- src/bittrex/api/public.clj
+++ src/bittrex/api/public.clj
@@ -3,17 +3,15 @@
             [clj-http.client :as client]
             [uri.core :as uri]))
 
-(str (uri/map->uri {:scheme "https"
-                    :host "bittrex.com"
-                    :path "/api/v1.1/public/getticker"
-                    :query {:market "BTC-LTC"}}))
-
 (defn- public-get
   ([tail]
-   (json/parse-string (:body (client/get
-                              (str (uri/map->uri {:scheme "https"
-                                                  :host "bittrex.com"
-                                                  :path (str "/api/v1.1/public/" tail)})))))))
+   (public-get tail nil))
+  ([tail query]
+   (-> {:scheme "https"
+        :host "bittrex.com"
+        :path (str "/api/v1.1/public/" tail)
+        :query query}
+       uri/map->uri str client/get :body json/parse-string)))
 
 (defn get-markets []
   (public-get "getmarkets"))
@@ -22,15 +20,18 @@
   (public-get "getcurrencies"))
 
 (defn get-ticker [market]
-  (public-get (str "getticker?market=" market)))
+  (public-get "getticker"
+              {:market market}))
 
 (defn get-market-summaries []
   (public-get "getmarketsummaries"))
 
 (defn get-market-summary [market]
-  (public-get (str "getmarketsummary?market=" market)))
+  (public-get "getmarketsummary"
+              {:market market}))
 
 (defn get-order-book [market type])
 
 (defn get-market-history [market]
-  (public-get (str "getmarkethistory?market=" market)))
+  (public-get "getmarkethistory"
+              {:market market}))
