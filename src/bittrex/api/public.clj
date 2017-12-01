9ac5fa6ee30a75addf64364a7d5ea976b5dcf4e7
Christopher Mark Gore <chris.gore@climate.com>
2017-12-01 16:50:59 -0600

    get-order-book

--- src/bittrex/api/public.clj
+++ src/bittrex/api/public.clj
@@ -13,25 +13,46 @@
         :query query}
        uri/map->uri str client/get :body json/parse-string)))
 
-(defn get-markets []
+(defn get-markets
+  []
   (public-get "getmarkets"))
 
-(defn get-currencies []
+(defn get-currencies
+  []
   (public-get "getcurrencies"))
 
-(defn get-ticker [market]
+(defn get-ticker
+  [market]
   (public-get "getticker"
               {:market market}))
 
-(defn get-market-summaries []
+(defn get-market-summaries
+  []
   (public-get "getmarketsummaries"))
 
 (defn get-market-summary [market]
   (public-get "getmarketsummary"
               {:market market}))
 
-(defn get-order-book [market type])
+(defn get-order-book
+  [market type]
+  (public-get "getorderbook"
+              {:market market
+               :type type}))
 
-(defn get-market-history [market]
+(defn get-order-book-buy
+  [market]
+  (get-order-book market "buy"))
+
+(defn get-order-book-sell
+  [market]
+  (get-order-book market "sell"))
+
+(defn get-order-book-both
+  [market]
+  (get-order-book market "both"))
+
+(defn get-market-history
+  [market]
   (public-get "getmarkethistory"
               {:market market}))
