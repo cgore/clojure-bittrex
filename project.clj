1252ffcaf31d82466cbfadd7f7d0090d38c32659
Christopher Mark Gore <cgore@cgore.com>
2018-01-02 17:30:59 -0600

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.cgore/bittrex "0.1.0-SNAPSHOT"
+(defproject com.cgore/bittrex "0.1.0"
   :description "Clojure interface to the Bittrex.com API"
   :url "https://github.com/cgore/clojure-bittrex"
   :license {:name "BSD-3-Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
+  :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.8.0"] ; https://github.com/dakrone/cheshire
                  [clj-http "3.7.0"] ; https://github.com/dakrone/clj-http
