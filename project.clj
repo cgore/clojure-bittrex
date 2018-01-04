e7907dbab4db83c53987343f93a1c2542c082112
Christopher Mark Gore <cgore@cgore.com>
2018-01-04 12:23:29 -0600

    Clojure 1.9

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD-3-Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :repositories [["clojars" {:sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [cheshire "5.8.0"] ; https://github.com/dakrone/cheshire
                  [clj-http "3.7.0"] ; https://github.com/dakrone/clj-http
                  [uri "1.1.0"]]) ; https://github.com/codonnell/uri
